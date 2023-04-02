4ca544c5675f5d2d98aad0754bd57545c6f39835
Neil McLaughlin <neil@mittenview.co.uk>
2023-04-02 21:58:18 +0000

    Handle unknown countries and case sensitivity

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 function getCapitalMessage (country) {
-  return `The capital of ${country} is ${capitals[country]}`
+   const capital = capitals[country?.toLowerCase()] 
+   if (capital) {
+      return `The capital of ${capitalise(country)} is ${capitalise(capital)}`
+   }
+   return `The capital of ${capitalise(country)} is not known`
+}
+
+function capitalise(sentence) {
+   return sentence.split(" ")
+     .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+     .join(" ");
 }
 
 const server = http.createServer((req, res) => {
    res.statusCode = 200;
    res.setHeader('Content-Type', 'text/plain');
 
-   const parsedUrl = url.parse(req.url);
-   const country = parsedUrl.pathname.replace(/^\//, '');
-   const message = getCapitalMessage(country);
+   const query = url.parse(req.url, true).query;
+   const message = getCapitalMessage(query.country);
    res.end(message);
 });
 
