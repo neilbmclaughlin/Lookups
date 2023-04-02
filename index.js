e88669e466119f22aaa802837a5402264689574e
Neil McLaughlin <neil@mittenview.co.uk>
2023-04-02 12:06:06 +0000

    Create an API to return capital for a given country

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const http = require('http');
+const url = require('url');
+const capitals = require('./capitals.json')
+const hostname = '127.0.0.1';
+const port = 3000;
+
 function getCapitalMessage (country) {
   return `The capital of ${country} is ${capitals[country]}`
 }
 
-const capitals = require('./capitals.json')
-console.log(capitals)
-console.log(getCapitalMessage('England'))
-console.log(getCapitalMessage('France'))
-console.log(capitals.England)
+const server = http.createServer((req, res) => {
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/plain');
+
+   const parsedUrl = url.parse(req.url);
+   const country = parsedUrl.pathname.replace(/^\//, '');
+   const message = getCapitalMessage(country);
+   res.end(message);
+});
+
+server.listen(port, hostname, () => {
+   console.log(capitals)
+   console.log(`Server running at http://${hostname}:${port}/`);
+});
